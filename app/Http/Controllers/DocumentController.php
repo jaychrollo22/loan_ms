2fb294df53dba900f089a4ec66ddf982ed2d3b5a
Devy Viray <demetrio.viray@lfuggoc.net>
2023-12-30 00:19:18 +0800

    Uploading and viewing of Documents

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Auth;
+use Storage;
+use Illuminate\Http\Request;
+use App\{
+    Document
+};
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'photo' => 'required'
+        ],[
+            'photo.required' => 'Attachment field is required'
+        ]);
+
+        DB::beginTransaction();
+        try {
+            $document = Document::create($request->all() + ['borrower_id' => '6']);
+            //Saving of uploaded photo
+            $document->update([
+                'file_path' => Storage::disk('public')->put('Borrowers/'.'ID-6/Documents', $request->photo),
+                'file_name' => $request->photo->getClientOriginalName(),
+            ]);
+
+            DB::commit();
+            return $document;
+        } catch (Exception $e) {
+            DB::rollBack();
+            // return GlobalController::errorLogs($e->getMessage(),'App\DocumentRfp');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Document $document)
+    {
+        return $document->delete();
+    }
+
+    /**
+     * Display all resources.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function lists(){
+        return Document::get();
+    }
+}
---
+++ database/migrations/2023_12_29_221749_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->integer('borrower_id');
+            $table->char('title',80);
+            $table->char('file_name',80)->nullable();
+            $table->string('file_path')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
